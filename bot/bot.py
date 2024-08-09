923fa079a49058933d4546978289cf2140828fc8
sen-sourav <souravsen1990@gmail.com>
2024-08-08 19:21:57 -0500

    discord bugfix

--- bot/bot.py
+++ bot/bot.py
@@ -1,39 +1,71 @@
 import discord
+from discord.ext import commands
 import requests
 import os
+from app.model_inference import model_inference
+from dotenv import load_dotenv
 
-TOKEN = 'MTI3MDEyMjkzMzM3NzA0NDY1Mg.G36aMp.eMe7mscWfnkOayHyhkIOtWmlliUSo_0rpJ6NqM'  # Replace with your bot's token
-server_ip = '0.0.0.0' # replace with server ip
-client = discord.Client()
+TOKEN = 'REDACTED'  # Replace with your bot's token
 
-@client.event
+
+# # Define a subclass of commands.Bot
+class MyBot(commands.Bot):
+    def __init__(self):
+        intents = discord.Intents.default()
+        intents.message_content = True  # Enable message content intent
+        model = None
+        super().__init__(command_prefix="!", intents=intents)
+
+    async def setup_hook(self):
+        # Sync the commands with the Discord server
+        await self.tree.sync()
+        print("Commands synced.")
+
+
+# Initialize the bot
+bot = MyBot()
+
+# bot = commands.Bot(command_prefix="!", intents=discord.Intents.default())
+bot.tree.sync()
+
+
+#  Define an event for when the bot is ready
+@bot.event
 async def on_ready():
-    print(f'Logged in as {client.user}')
+    print(f'Logged in as {bot.user} (ID: {bot.user.id})')
+    print('------')
 
-@client.event
+
+# Define the /SingSong command
+@bot.tree.command(name="singsong")
+async def singsong(interaction: discord.Interaction):
+    await interaction.response.send_message(
+        "Please join a voice channel and sing your song. You can then upload the recording here!")
+
+
+@bot.event
 async def on_message(message):
-    if message.author == client.user:
+    # Ignore messages by bot, this prevents infinite loop
+    if message.author == bot.user:
         return
 
-    if message.content.startswith('!process'):
-        if len(message.attachments) > 0:
-            attachment = message.attachments[0]
-            input_file_path = f"uploads/{attachment.filename}"
-            os.makedirs(os.path.dirname(input_file_path), exist_ok=True)
-            await attachment.save(input_file_path)
+    if message.attachments:
+        attachment = message.attachments[0]
+        if attachment.filename.endswith(('mp3', 'wav', 'ogg', 'm4a')):
+            await message.channel.send(f"Received voice recording: {message.author.name}_{attachment.filename}")
+            # You can save or process the file here
+            filename = f"{message.author.name}_{attachment.filename}"
+            await attachment.save(f'/home/ubuntu/prod_code/uploads/{filename}')
+            input_file_path = f"/home/ubuntu/prod_code/uploads/{filename}"
+            output_dir = "/home/ubuntu/prod_code/output/"
+            model_inference(bot.model, input_file_path, output_dir)
+            basename = filename.split('.')[-2]
+            file = discord.File(
+                f'/home/ubuntu/prod_code/output/{basename}_final_mix.wav')  # replace with your file path
+            await message.channel.send(file=file)
+    await bot.process_commands(message)
 
-            files = {'file': open(input_file_path, 'rb')}
-            response = requests.post(f'http://{server_ip}:8000/inference', files=files)
 
-            if response.status_code == 200:
-                response_data = response.json()
-                if 'output_file' in response_data:
-                    output_file_path = response_data['output_file']
-                    await message.channel.send(file=discord.File(output_file_path))
-                else:
-                    await message.channel.send("There was an error processing your file.")
-            else:
-                await message.channel.send("There was an error processing your file.")
-
-def run_discord_bot():
-    client.run(TOKEN)
+def run_discord_bot(model=None):
+    bot.model = model
+    bot.run(TOKEN)
--- bot/main.py
+++ bot/main.py
@@ -1,10 +1,6 @@
+import bot
 from app.model_loader import load_model
-from app.model_inference import model_inference
 
 if __name__ == '__main__':
     model = load_model()
-    input_file_path = "/home/supers/SAG/TTTTT/prod_code/uploads/audio_6.wav"
-    output_dir = "/home/supers/SAG/TTTTT/prod_code/output/"
-
-    model_inference(model, input_file_path, output_dir)
-
+    bot.run_discord_bot(model=model)
